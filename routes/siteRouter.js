08ebc71b7f4980acc7752b6c51ebec7754def141
Gavin O Brien <142741214+Gaviobm1@users.noreply.github.com>
2024-08-08 19:36:38 +0200

    Add basic functionality

--- routes/siteRouter.js
+++ routes/siteRouter.js
@@ -1,8 +1,54 @@
 const express = require("express");
 const router = express.Router();
 
+const num = 0;
+
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date(),
+    id: 1,
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date(),
+    id: 2,
+  },
+];
+
+const links = [
+  {
+    href: "/",
+    text: "Home",
+  },
+  {
+    href: "/new",
+    text: "New Message",
+  },
+];
+
 router.get("/", (req, res, next) => {
-  res.render("index");
+  res.render("index", { messages, links });
+});
+
+router.get("/new", (req, res, next) => {
+  res.render("form", { links });
+});
+
+router.post("/new", (req, res, next) => {
+  messages.push({
+    text: req.body.message,
+    user: req.body.user,
+    added: new Date(),
+    id: messages.length + 1,
+  });
+  res.redirect("/");
+});
+
+router.get("/new/:id", (req, res, next) => {
+  res.render("message", { message: messages[req.params.id - 1], links });
 });
 
 module.exports = router;
